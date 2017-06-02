999d739b5d28b50bb92a81ebcd16005ebab6ff2a
Ali Kefia <ali.kefia@coopengo.com>
2017-06-02 16:05:57 +0200

    use standard - format and lint

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-var _ = require('lodash');
-var moment = require('moment');
-//
-exports.decimal = function (n) {
-  return new Number(n);
-};
-exports.isDecimal = function (n) {
-  return n instanceof Number;
-};
-//
-function Binary(base64) {
-  this.base64 = base64.replace(/\s/g, '');
+var _ = require('lodash')
+var moment = require('moment')
+
+
+function Decimal (number) {
+  this.number = number
 }
+
+Decimal.prototype.get = function () {
+  return this.number
+}
+
+exports.decimal = function (n) {
+  return new Decimal(n)
+}
+
+exports.isDecimal = function (n) {
+  return n instanceof Decimal
+}
+
+
+function Binary (base64) {
+  this.base64 = base64.replace(/\s/g, '')
+}
+
+Binary.prototype.get = function () {
+  return this.base64
+}
+
 exports.binary = function (base64) {
-  return new Binary(base64);
-};
+  return new Binary(base64)
+}
+
 exports.isBinary = function (b) {
-  return b instanceof Binary;
-};
-//
-var DATE_FORMAT = 'YYYY-MM-DD';
-exports.DATE_FORMAT = DATE_FORMAT;
+  return b instanceof Binary
+}
+
+
+var DATE_FORMAT = 'YYYY-MM-DD'
+exports.DATE_FORMAT = DATE_FORMAT
+
 exports.date = function (y, M, D) {
   // nil values are replaced by current
-  var r;
+  var r
   if (_.isString(y)) {
-    r = moment(y, DATE_FORMAT);
-    r.startOf('day');
-  }
-  else {
+    r = moment(y, DATE_FORMAT)
+    r.startOf('day')
+  } else {
     var values = {
       y: y,
       M: M,
@@ -36,63 +54,66 @@ exports.date = function (y, M, D) {
       m: 0,
       s: 0,
       ms: 0
-    };
-    values = _.pickBy(values, (v) => !_.isNil(v));
-    r = moment();
-    r.set(values);
+    }
+    values = _.pickBy(values, (v) => !_.isNil(v))
+    r = moment()
+    r.set(values)
   }
-  r.isDate = true;
-  return r;
-};
+  r.isDate = true
+  return r
 
-function isDate(d) {
-  return moment.isMoment(d) && d.isDate;
 }
-exports.isDate = isDate;
-//
-var TIME_FORMAT = 'HH:mm:ss.SSS';
-exports.TIME_FORMAT = TIME_FORMAT;
+
+function isDate (d) {
+  return moment.isMoment(d) && d.isDate
+}
+exports.isDate = isDate
+
+
+var TIME_FORMAT = 'HH:mm:ss.SSS'
+exports.TIME_FORMAT = TIME_FORMAT
+
 exports.time = function (h, m, s, ms) {
   // nil values are replaced by current
   var empty = {
     y: 0,
     M: 0,
     D: 1
-  };
-  var r;
-  if (_.isString(h)) {
-    r = moment(h, TIME_FORMAT);
-    r.set(empty);
   }
-  else {
+  var r
+  if (_.isString(h)) {
+    r = moment(h, TIME_FORMAT)
+    r.set(empty)
+  } else {
     var values = _.assign({
       h: h,
       m: m,
       s: s,
       ms: ms
-    }, empty);
-    r = moment();
-    r.set(_.pickBy(values, (v) => !_.isNil(v)));
+    }, empty)
+    r = moment()
+    r.set(_.pickBy(values, (v) => !_.isNil(v)))
   }
-  r.isTime = true;
-  return r;
-};
-
-function isTime(t) {
-  return moment.isMoment(t) && t.isTime;
+  r.isTime = true
+  return r
 }
-exports.isTime = isTime;
-//
-var DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS';
-exports.DATETIME_FORMAT = DATETIME_FORMAT;
+
+function isTime (t) {
+  return moment.isMoment(t) && t.isTime
+}
+exports.isTime = isTime
+
+
+var DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss.SSS'
+exports.DATETIME_FORMAT = DATETIME_FORMAT
+
 exports.datetime = function (y, M, D, h, m, s, ms, utc) {
   // nil values are replaced by current
-  var cls = utc ? moment.utc : moment;
-  var r;
+  var cls = utc ? moment.utc : moment
+  var r
   if (_.isString(y)) {
-    r = moment(y, DATETIME_FORMAT);
-  }
-  else {
+    r = moment(y, DATETIME_FORMAT)
+  } else {
     var values = {
       y: y,
       M: M,
@@ -101,25 +122,25 @@ exports.datetime = function (y, M, D, h, m, s, ms, utc) {
       m: m,
       s: s,
       ms: ms
-    };
-    r = cls();
-    r.set(_.pickBy(values, (v) => !_.isNil(v)));
+    }
+    r = cls()
+    r.set(_.pickBy(values, (v) => !_.isNil(v)))
   }
-  r.isDateTime = true;
-  return r.local();
-};
-
-function isDateTime(dt) {
-  return moment.isMoment(dt) && dt.isDateTime;
+  r.isDateTime = true
+  return r.local()
 }
-exports.isDateTime = isDateTime;
-//
+function isDateTime (dt) {
+  return moment.isMoment(dt) && dt.isDateTime
+}
+
+exports.isDateTime = isDateTime
+
+
 exports.timedelta = function (y, M, d, h, m, s, ms) {
-  var r;
+  var r
   if (_.isString(y)) {
-    r = moment.duration(y);
-  }
-  else {
+    r = moment.duration(y)
+  } else {
     var values = {
       y: y,
       M: M,
@@ -128,35 +149,31 @@ exports.timedelta = function (y, M, d, h, m, s, ms) {
       m: m,
       s: s,
       ms: ms
-    };
-    r = moment.duration(_.pickBy(values, (v) => !_.isNil(v)));
+    }
+    r = moment.duration(_.pickBy(values, (v) => !_.isNil(v)))
   }
-  r.isTimeDelta = true;
-  return r;
-};
-
-function isTimeDelta(td) {
-  return moment.isDuration(td) && td.isTimeDelta;
+  r.isTimeDelta = true
+  return r
 }
-exports.isTimeDelta = isTimeDelta;
-//
+
+function isTimeDelta (td) {
+  return moment.isDuration(td) && td.isTimeDelta
+}
+exports.isTimeDelta = isTimeDelta
+
+
 exports.stringify = function (data) {
   if (_.isNil(data) || _.isString(data)) {
-    return data;
+    return data
+  } else if (isDate(data)) {
+    return data.format(DATE_FORMAT)
+  } else if (isTime(data)) {
+    return data.format(TIME_FORMAT)
+  } else if (isDateTime(data)) {
+    return data.format(DATETIME_FORMAT)
+  } else if (isTimeDelta(data)) {
+    return data.toJSON()
+  } else {
+    throw new Error('unsupported data type')
   }
-  else if (isDate(data)) {
-    return data.format(DATE_FORMAT);
-  }
-  else if (isTime(data)) {
-    return data.format(TIME_FORMAT);
-  }
-  else if (isDateTime(data)) {
-    return data.format(DATETIME_FORMAT);
-  }
-  else if (isTimeDelta(data)) {
-    return data.toJSON();
-  }
-  else {
-    throw new Error('unsupported data type');
-  }
-};
+}
--- tests/constructors.js
+++ tests/constructors.js
@@ -1,128 +1,128 @@
-var moment = require('moment');
-var types = require('..');
-var t = require('tap');
+var moment = require('moment')
+var types = require('..')
+var t = require('tap')
 
-function testDecimal() {
-  var d = types.decimal(23);
-  t.ok(types.isDecimal(d));
-  t.equal(d.toString(), '23');
-  t.equal(Number(d), 23);
+function testDecimal () {
+  var d = types.decimal(23)
+  t.ok(types.isDecimal(d))
+  t.equal(d.get(), 23)
 }
 
-function testBinary() {
-  var d = types.binary('D D ');
-  t.ok(types.isBinary(d));
-  t.equal(d.base64, 'DD');
+function testBinary () {
+  var d = types.binary('D D ')
+  t.ok(types.isBinary(d))
+  t.equal(d.get(), 'DD')
 }
 
-function testDate() {
-  t.equal(types.DATE_FORMAT, 'YYYY-MM-DD');
-  var d;
-  //
-  d = types.date();
-  var now = moment();
-  t.ok(types.isDate(d));
-  t.equal(d.year(), now.year());
-  t.equal(d.month(), now.month());
-  t.equal(d.date(), now.date());
-  //
-  d = types.date(2010, 0, 1);
-  t.ok(types.isDate(d));
-  t.equal(types.stringify(d), '2010-01-01');
-  //
-  d = types.date('2016-12-31');
-  t.ok(types.isDate(d));
-  t.equal(types.stringify(d), '2016-12-31');
+function testDate () {
+  t.equal(types.DATE_FORMAT, 'YYYY-MM-DD')
+  var d
+
+  d = types.date()
+  var now = moment()
+  t.ok(types.isDate(d))
+  t.equal(d.year(), now.year())
+  t.equal(d.month(), now.month())
+  t.equal(d.date(), now.date())
+
+  d = types.date(2010, 0, 1)
+  t.ok(types.isDate(d))
+  t.equal(types.stringify(d), '2010-01-01')
+
+  d = types.date('2016-12-31')
+  t.ok(types.isDate(d))
+  t.equal(types.stringify(d), '2016-12-31')
 }
 
-function testTime() {
-  t.equal(types.TIME_FORMAT, 'HH:mm:ss.SSS');
-  var tm;
-  //
-  tm = types.time();
-  var now = moment();
-  t.ok(types.isTime(tm));
-  t.equal(tm.hours(), now.hours());
-  t.equal(tm.minutes(), now.minutes());
-  t.equal(tm.seconds(), now.seconds());
-  var delta = now.milliseconds() - tm.milliseconds();
-  t.ok(delta >= 0 && delta < 3);
-  //
-  tm = types.time(22, 59, 1, 234);
-  t.ok(types.isTime(tm));
-  t.equal(types.stringify(tm), '22:59:01.234');
-  //
-  tm = types.time('09:15:00.999');
-  t.ok(types.isTime(tm));
-  t.equal(types.stringify(tm), '09:15:00.999');
+function testTime () {
+  t.equal(types.TIME_FORMAT, 'HH:mm:ss.SSS')
+  var tm
+
+  tm = types.time()
+  var now = moment()
+  t.ok(types.isTime(tm))
+  t.equal(tm.hours(), now.hours())
+  t.equal(tm.minutes(), now.minutes())
+  t.equal(tm.seconds(), now.seconds())
+  var delta = now.milliseconds() - tm.milliseconds()
+  t.ok(delta >= 0 && delta < 3)
+
+  tm = types.time(22, 59, 1, 234)
+  t.ok(types.isTime(tm))
+  t.equal(types.stringify(tm), '22:59:01.234')
+
+  tm = types.time('09:15:00.999')
+  t.ok(types.isTime(tm))
+  t.equal(types.stringify(tm), '09:15:00.999')
 }
 
-function testDateTime() {
-  t.equal(types.DATETIME_FORMAT, 'YYYY-MM-DD HH:mm:ss.SSS');
-  var dt;
-  //
-  dt = types.datetime();
-  var now = moment();
-  t.ok(types.isDateTime(dt));
-  t.equal(dt.year(), now.year());
-  t.equal(dt.month(), now.month());
-  t.equal(dt.date(), now.date());
-  t.equal(dt.hours(), now.hours());
-  t.equal(dt.minutes(), now.minutes());
-  t.equal(dt.seconds(), now.seconds());
-  var delta = now.milliseconds() - dt.milliseconds();
-  t.ok(delta >= 0 && delta < 3);
-  //
-  dt = types.datetime(2016, 11, 31, 23, 59, 59, 999);
-  t.ok(types.isDateTime(dt));
-  t.equal(types.stringify(dt), '2016-12-31 23:59:59.999');
-  //
-  dt = types.datetime('2015-06-18 22:23:24.874');
-  t.ok(types.isDateTime(dt));
-  t.equal(types.stringify(dt), '2015-06-18 22:23:24.874');
-  //
-  dt = types.datetime(2010, 0, 1, 0, 0, 0, 0, true);
-  t.ok(types.isDateTime(dt));
-  t.equal(dt.hours(), 1);
-  //
-  dt = types.datetime(2010, 6, 1, 0, 0, 0, 0, true);
-  t.ok(types.isDateTime(dt));
-  t.equal(dt.hours(), 2);
+function testDateTime () {
+  t.equal(types.DATETIME_FORMAT, 'YYYY-MM-DD HH:mm:ss.SSS')
+  var dt
+
+  dt = types.datetime()
+  var now = moment()
+  t.ok(types.isDateTime(dt))
+  t.equal(dt.year(), now.year())
+  t.equal(dt.month(), now.month())
+  t.equal(dt.date(), now.date())
+  t.equal(dt.hours(), now.hours())
+  t.equal(dt.minutes(), now.minutes())
+  t.equal(dt.seconds(), now.seconds())
+  var delta = now.milliseconds() - dt.milliseconds()
+  t.ok(delta >= 0 && delta < 3)
+
+  dt = types.datetime(2016, 11, 31, 23, 59, 59, 999)
+  t.ok(types.isDateTime(dt))
+  t.equal(types.stringify(dt), '2016-12-31 23:59:59.999')
+
+  dt = types.datetime('2015-06-18 22:23:24.874')
+  t.ok(types.isDateTime(dt))
+  t.equal(types.stringify(dt), '2015-06-18 22:23:24.874')
+
+  dt = types.datetime(2010, 0, 1, 0, 0, 0, 0, true)
+  t.ok(types.isDateTime(dt))
+  t.equal(dt.hours(), 1)
+
+  dt = types.datetime(2010, 6, 1, 0, 0, 0, 0, true)
+  t.ok(types.isDateTime(dt))
+  t.equal(dt.hours(), 2)
 }
 
-function testTimeDelta() {
-  var td;
-  //
-  td = types.timedelta(1, 2, 3, 4, 5, 6, 7);
-  t.ok(types.isTimeDelta(td));
-  var str = types.stringify(td);
-  td = types.timedelta(str);
-  t.ok(types.isTimeDelta(td));
-  t.equal(td.years(), 1);
-  t.equal(td.months(), 2);
-  t.equal(td.days(), 3);
-  t.equal(td.hours(), 4);
-  t.equal(td.minutes(), 5);
-  t.equal(td.seconds(), 6);
-  t.equal(td.milliseconds(), 7);
-  //
-  td = types.timedelta(1);
-  var now = moment();
-  var year = now.year();
-  now.add(td);
-  t.equal(now.year(), year + 1);
+function testTimeDelta () {
+  var td
+
+  td = types.timedelta(1, 2, 3, 4, 5, 6, 7)
+  t.ok(types.isTimeDelta(td))
+  var str = types.stringify(td)
+  td = types.timedelta(str)
+  t.ok(types.isTimeDelta(td))
+  t.equal(td.years(), 1)
+  t.equal(td.months(), 2)
+  t.equal(td.days(), 3)
+  t.equal(td.hours(), 4)
+  t.equal(td.minutes(), 5)
+  t.equal(td.seconds(), 6)
+  t.equal(td.milliseconds(), 7)
+
+  td = types.timedelta(1)
+  var now = moment()
+  var year = now.year()
+  now.add(td)
+  t.equal(now.year(), year + 1)
 }
 
-function testStringify() {
-  t.equal(types.stringify(null), null);
-  t.equal(types.stringify(''), '');
-  t.throws(types.stringify.bind(null, moment()));
-  t.throws(types.stringify.bind(null, moment.duration()));
+function testStringify () {
+  t.equal(types.stringify(null), null)
+  t.equal(types.stringify(''), '')
+  t.throws(types.stringify.bind(null, moment()))
+  t.throws(types.stringify.bind(null, moment.duration()))
 }
-testDecimal();
-testBinary();
-testDate();
-testTime();
-testDateTime();
-testTimeDelta();
-testStringify();
+
+testDecimal()
+testBinary()
+testDate()
+testTime()
+testDateTime()
+testTimeDelta()
+testStringify()
